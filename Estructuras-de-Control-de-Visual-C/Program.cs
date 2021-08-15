9822e72775e150319a278fbdb0700a23a6a2dc7e
Joan Steven Gómez Contreras <joanstevengomezcontreras@outlook.com>
2021-08-15 13:18:14 -0600

    Agregar archivos de proyecto.

---
+++ Estructuras-de-Control-de-Visual-C/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace semana3_estructuras_de_control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            principal();
+
+        }
+
+        static void principal()
+        {
+            int repetir_menu;
+            System.Console.WriteLine("Estructuras de control en Visual C#");
+            System.Console.WriteLine("1. Menú de opciones");
+            System.Console.WriteLine("2. Salir");
+
+            System.Console.WriteLine("Elija 1 para mostrar menú o 2 para salir. ");
+            repetir_menu = Int32.Parse(Console.ReadLine());
+
+            while (repetir_menu == 1)
+            {
+                opciones();
+            }
+
+
+        }
+
+
+        static void opciones()
+        {
+
+            int opcion;
+
+            System.Console.WriteLine("Ejemplos de estructuras de control");
+            System.Console.WriteLine("Opcion 1");
+            System.Console.WriteLine("Opcion 2");
+            System.Console.WriteLine("Opcion 3");
+            System.Console.WriteLine("Opcion 4");
+
+            System.Console.WriteLine("Elija una opcion: ");
+            opcion = Int32.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    programa1();
+                    break;
+                case 2:
+                    programa2();
+                    break;
+                case 3:
+                    programa3();
+                    break;
+                case 4:
+                    programa4();
+                    break;
+                default:
+                    System.Console.WriteLine("La opcion que eligio no exixte");
+                    break;
+            }
+            //System.Console.ReadKey();
+            principal();
+
+        }
+
+        static void programa1()
+        {
+            System.Console.WriteLine("Opcion 1");
+
+        }
+
+        static void programa2()
+        {
+            System.Console.WriteLine("Opcion 2");
+
+        }
+
+        static void programa3()
+        {
+            System.Console.WriteLine("Opcion 3");
+
+        }
+
+        static void programa4()
+        {
+            System.Console.WriteLine("Opcion 4");
+        }
+
+    }
+}
